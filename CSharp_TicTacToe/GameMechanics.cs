923322fcd678aed5be12a1e96f0b46ee41a5ce5c
Michael E Miles <michael@miles-me.net>
2017-07-15 19:09:03 -0500

    Massive If statment with and's and or's

--- CSharp_TicTacToe/GameMechanics.cs
+++ CSharp_TicTacToe/GameMechanics.cs
@@ -11,6 +11,7 @@ namespace CSharp_TicTacToe
             Console.WriteLine(Winner(board, player, marker));
         }
 
+        //massive if statement with and's and or's
         private string Winner(Dictionary<string, string> chkBoard, string player, string marker)
         {
             if (chkBoard["top_L"] == marker && chkBoard["top_M"] == marker && chkBoard["top_R"] == marker ||
