930a87f94cfd84373e98450a5064d14af41b6a87
Michael E Miles <michael@miles-me.net>
2017-07-19 03:01:52 -0500

    Inverted IF statements, converted bool return methods to a return method only with out IF's

--- CSharp_TicTacToe/GameMechanics.cs
+++ CSharp_TicTacToe/GameMechanics.cs
@@ -12,15 +12,14 @@ namespace CSharp_TicTacToe
             var rowInput = Console.ReadLine() ?? "";
             rowInput = rowInput.ToLower();
 
-            if (rowInput != "" && (rowInput == "top" || rowInput == "mid" || rowInput == "low"))
-            {
-                Console.Write("\nPlease enter Column: (L)eft, (M)iddle, (R)ight: ");
-                var colInput = Console.ReadLine()?? "";
-                colInput = colInput.ToUpper();
+            if (rowInput == "" || (rowInput != "top" && rowInput != "mid" && rowInput != "low")) return "";
 
-                if (colInput != "" && colInput.Length == 1 && (colInput == "L" || colInput == "M" || colInput == "R"))
-                    return rowInput + "_" + colInput;
-            }
+            Console.Write("\nPlease enter Column: (L)eft, (M)iddle, (R)ight: ");
+            var colInput = Console.ReadLine()?? "";
+            colInput = colInput.ToUpper();
+
+            if (colInput != "" && colInput.Length == 1 && (colInput == "L" || colInput == "M" || colInput == "R"))
+                return rowInput + "_" + colInput;
             return "";
         }
 
@@ -34,18 +33,14 @@ namespace CSharp_TicTacToe
 
         public bool WinCheck(Dictionary<string, string> board, string marker)
         {
-            if (board["top_L"] == marker && board["top_M"] == marker && board["top_R"] == marker ||
-                board["mid_L"] == marker && board["mid_M"] == marker && board["mid_R"] == marker ||
-                board["low_L"] == marker && board["low_M"] == marker && board["low_R"] == marker ||
-                board["top_L"] == marker && board["mid_L"] == marker && board["low_L"] == marker ||
-                board["top_M"] == marker && board["mid_M"] == marker && board["low_M"] == marker ||
-                board["top_R"] == marker && board["mid_R"] == marker && board["low_R"] == marker ||
-                board["top_L"] == marker && board["mid_M"] == marker && board["low_R"] == marker ||
-                board["low_L"] == marker && board["mid_M"] == marker && board["top_R"] == marker)
-            {
-                return true;
-            }
-            return false;
+            return board["top_L"] == marker && board["top_M"] == marker && board["top_R"] == marker ||
+                   board["mid_L"] == marker && board["mid_M"] == marker && board["mid_R"] == marker ||
+                   board["low_L"] == marker && board["low_M"] == marker && board["low_R"] == marker ||
+                   board["top_L"] == marker && board["mid_L"] == marker && board["low_L"] == marker ||
+                   board["top_M"] == marker && board["mid_M"] == marker && board["low_M"] == marker ||
+                   board["top_R"] == marker && board["mid_R"] == marker && board["low_R"] == marker ||
+                   board["top_L"] == marker && board["mid_M"] == marker && board["low_R"] == marker ||
+                   board["low_L"] == marker && board["mid_M"] == marker && board["top_R"] == marker;
         }
     }
 }
